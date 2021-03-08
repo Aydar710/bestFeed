63afedc30ecd446d444cea0073904811dc4782bf
Aydar <aydar710@gmail.com>
2021-03-08 17:28:52 +0300

    init

---
+++ app/src/main/java/com/aydar/bestfeed/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aydar.bestfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
