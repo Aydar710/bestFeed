3db59351fbed63ec8ba8f5606f07a9ac109f2a61
Aydar <aydar710@gmail.com>
2021-03-13 13:33:16 +0300

    add buildSrc

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.kotlin.konan.properties.loadProperties
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+
+    compileSdkVersion(DefaultConfig.compileSdkVersion)
+    buildToolsVersion(DefaultConfig.buildToolsVersion)
+
+    defaultConfig {
+
+        applicationId = DefaultConfig.applicationId
+
+        minSdkVersion(DefaultConfig.minSdkVersion)
+        targetSdkVersion(DefaultConfig.targetSdkVersion)
+
+        versionCode = DefaultConfig.versionCode
+        versionName = DefaultConfig.versionName
+
+        testInstrumentationRunner = DefaultConfig.testInstrumentationRunner
+    }
+
+
+    signingConfigs {
+        create("release") {
+
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+        }
+
+        getByName("debug") {
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    kotlinOptions.jvmTarget = "1.8"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "BestFeed"
+include(
+    ":app"
+)
